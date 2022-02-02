a055c34e3571a402edd4ecc66234673f3f63af91
kenke11 <kenkebashvili2@gmail.com>
2022-02-02 14:22:14 +0400

    style: language change nav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React from "react";
+import React, {Fragment} from "react";
 
 function App() {
   return (
-    <div className="flex justify-center">
-      <h1 className='font-semibold text-3xl text-red-700'>Hello movie quotes</h1>
-    </div>
+    <Fragment>
+      <div class="fixed flex flex-col items-center justify-center h-screen ml-12">
+        <nav>
+          <ul>
+            <li className="text-2xl border border-white mt-3 rounded-full px-2 py-1.5 text-black bg-white cursor-pointer">EN</li>
+            <li className="text-2xl border border-white mt-3 rounded-full px-2 py-1.5 text-white cursor-pointer">KA</li>
+          </ul>
+        </nav>
+      </div>
+    </Fragment>
   );
 }
 
