d7c39675ea84d4aecf5c0cba0697ca4118ec9603
kenke11 <kenkebashvili2@gmail.com>
2022-02-15 16:08:19 +0400

    fix: return image in quotes page

--- src/components/quotes/Quote.js
+++ src/components/quotes/Quote.js
@@ -7,7 +7,7 @@ const Quote = ({ quote }) => {
     <div className='bg-white rounded-xl my-10 max-w-2xl m-auto'>
       <img
         className='flex justify-center text-center m-auto rounded-xl w-full'
-        src={`http://127.0.0.1:8000/storage/${quote.quote_img}`}
+        src={`https://movie-quotes-api.tazo.redberryinternship.ge/storage/${quote.quote_img}`}
         alt='quote img'
       />
       <div className='py-3 overflow-y-auto max-h-36'>
