981206598831231b764149938c77c440f5c8e277
kenke11 <kenkebashvili2@gmail.com>
2022-03-11 15:24:04 +0400

    refactor: use tailwind on 404 page

--- src/components/error/NotFound.js
+++ src/components/error/NotFound.js
@@ -1,10 +1,9 @@
-import classes from './NotFound.module.css';
 import { Link } from 'react-router-dom';
 
 const NotFound = () => {
   return (
-    <div className={classes.container}>
-      <div className={classes.text}>
+    <div className='h-screen overflow-hidden flex relative align-center justify-center  bg-gradient-to-r from-violet-800 to-blue-900'>
+      <div className='absolute top-1/4 text-white text-center'>
         <div>ERROR</div>
         <h1>404</h1>
         <hr />
@@ -14,7 +13,7 @@ const NotFound = () => {
         </div>
       </div>
 
-      <div className={classes.astronaut}>
+      <div className='w-28 absolute top-1/2 transition animation-duration-500 animate-[astronaut_5s_ease-in-out_infinite]'>
         <img
           src='https://images.vexels.com/media/users/3/152639/isolated/preview/506b575739e90613428cdb399175e2c8-space-astronaut-cartoon-by-vexels.png'
           alt='astronaut'
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,17 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        astronaut: {
+          '0%': { left: '-100px' },
+          '25%': { transform: 'rotate(20deg)', top: '40%' },
+          '50%': { transform: 'rotate(70deg)', top: '55%' },
+          '75%': { transform: 'rotate(30deg)', top: '70%' },
+          '100%': { transform: 'rotate(45deg)', left: '100%' },
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
